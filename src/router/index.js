02d48aae6217a00908da39c042d1fe71e919c909
cyrnicolase <nicolase.chen@gmail.com>
2018-11-26 19:18:59 +0800

    去掉冗余的文件

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Calculator from '@/components/Calculator'
 import Duty from '@/components/Duty'
 
 Vue.use(Router)
@@ -11,16 +9,6 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/calculator',
-      name: 'calculator',
-      component: Calculator
-    },
-    {
-      path: '/duty',
       name: 'duty',
       component: Duty
     }
